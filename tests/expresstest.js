ed3d1422f88a21df5c03dddf2341848f0ef3a7cf
lwscroggins <larry.scroggins@gmail.com>
2014-08-15 13:28:45 -0700

    removing commented out code. sorry.

--- tests/expresstest.js
+++ tests/expresstest.js
@@ -1,12 +1,5 @@
 'use strict'
 
-// var casper = require('casper').create(
-//   {
-//     verbose: true,
-//     loglevel: 'debug'
-//   }
-// );
-
 casper.test.begin('tests that http status is 200', 1, function suite(test) {
   casper.start('http://localhost:3000/', function() {
     test.assertHttpStatus(200);
